9ee0ea0363f94f56dff372655074d7e808582859
ouuan <y___o___u@126.com>
2018-12-12 01:17:09 +0800

    Site updated: 2018-12-12 01:17:05

--- randpic/index.php
+++ randpic/index.php
@@ -1,4 +1,5 @@
-<span class="hljs-keyword">glob</span>(“./*.{gif,jpg,png}”,GLOB_BRACE);
- $img = array_rand($img_array);
- header(“location:$img_array[$img]”);
- ?&gt;
+<?php
+$img_array = glob(“./*.{gif,jpg,png}”,GLOB_BRACE);
+$img = array_rand($img_array);
+header(“location:$img_array[$img]”);
+?>
